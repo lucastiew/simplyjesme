0e73cd367ae87a4771b39b60e2142a8ef13ad0e5
lucastiew <lucastiew@users.noreply.github.com>
2016-03-13 22:50:41 +0800

    Update simplyjesme_script_test.js

--- simplyjesme_script_test.js
+++ simplyjesme_script_test.js
@@ -86,7 +86,7 @@ function printRelatedLabels_thumbs(e){
 	var n=0;
 	if(relatedTitles.length>0)
 		document.write('<div class="title"><h4>'+relatedpoststitle+"</h4></div>");
-	document.write('<div class="owl-carousel" id="owl-demo"/>');
+	document.write('<div class="owl-carousel owl-theme" id="owl-demo"/>');
 	while(n<relatedTitles.length&&n<20&&n<maxresults){
 		document.write('<div class="item"><a ');
 		if(n!=0)
